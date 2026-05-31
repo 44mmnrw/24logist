02fa76240bd9f341e820be2566b18f9740f871bc
44mmnrw <44mm.nrw@gmail.com>
2026-05-31 11:20:05 +0300

    deploy: update

--- app/Support/LandingIcons.php
+++ app/Support/LandingIcons.php
@@ -88,6 +88,7 @@ final class LandingIcons
         'menu-dots' => 'Меню',
         'mail' => 'Email',
         'phone' => 'Телефон',
+        'tech-support' => 'Техподдержка',
     ];
 
     public static function resolve(?string $value): ?string
